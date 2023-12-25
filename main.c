a8113ef4700842944a5c6578564a306409c7bf83
Pessi <--add>
2023-12-25 18:59:43 +0200

    -

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
+//TODO: import important imports
 
 int main(int args, char * argv[]) {
 	printf("The last shell shit you should even look at ever. Quit now.\n");
+	// TODO: insert references to other projects here
 }
